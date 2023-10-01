6dc418370a305e4e16ff23bbda640286f71d3bbb
Flippie Coetser <flippie.coetser@gmail.com>
2023-10-01 09:28:38 -0700

    EXCEPTIONS: Exceptions() ReturnsListContainingEntity.NotFound -> PASS

--- R/Exceptions.R
+++ R/Exceptions.R
@@ -1,9 +1,10 @@
 Exceptions <- \() {
   exceptions <- list()
-  exceptions[['Type.Invalid']]   <- \() { }
-  exceptions[['Type.Mismatch']]  <- \() { }
-  exceptions[['Value.Invalid']]  <- \() { }
-  exceptions[['Value.NULL']]     <- \() { }
-  exceptions[['Entity.Invalid']] <- \() { }
+  exceptions[['Type.Invalid']]    <- \() { }
+  exceptions[['Type.Mismatch']]   <- \() { }
+  exceptions[['Value.Invalid']]   <- \() { }
+  exceptions[['Value.NULL']]      <- \() { }
+  exceptions[['Entity.Invalid']]  <- \() { }
+  exceptions[['Entity.NotFound']] <- \() { }
   return(exceptions)
 }
