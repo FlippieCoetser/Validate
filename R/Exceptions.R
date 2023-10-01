f0b687c2c9506412e8d87c771c16c89f18187ff2
Flippie Coetser <flippie.coetser@gmail.com>
2023-10-01 09:27:32 -0700

    EXCEPTIONS: Exceptions() ReturnsListContainingEntity.Invalid -> PASS

--- R/Exceptions.R
+++ R/Exceptions.R
@@ -1,8 +1,9 @@
 Exceptions <- \() {
   exceptions <- list()
-  exceptions[['Type.Invalid']]  <- \() {}
-  exceptions[['Type.Mismatch']] <- \() { }
-  exceptions[['Value.Invalid']] <- \() { }
-  exceptions[['Value.NULL']]    <- \() { }
+  exceptions[['Type.Invalid']]   <- \() { }
+  exceptions[['Type.Mismatch']]  <- \() { }
+  exceptions[['Value.Invalid']]  <- \() { }
+  exceptions[['Value.NULL']]     <- \() { }
+  exceptions[['Entity.Invalid']] <- \() { }
   return(exceptions)
 }
