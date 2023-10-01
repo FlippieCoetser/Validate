63be5508babf189115181e8685ddd96ef2f9f0d1
Flippie Coetser <flippie.coetser@gmail.com>
2023-10-01 10:27:31 -0700

    VALIDATION: Validator() ReturnsListOfValidators -> FAIL

--- tests/testthat/test-Validator.R
+++ tests/testthat/test-Validator.R
@@ -3,3 +3,13 @@ describe('Validator',{
     Validator |> expect.exist()
   })
 })
+
+describe("When validations <- Validator()",{
+  it('then validations is a list',{
+    # When
+    validations <- Validator()
+
+    # Then
+    validations |> expect.list()
+  })
+})
