634ab72721f7fb68fe71e369349e4ad1edb7128d
Flippie Coetser <flippie.coetser@gmail.com>
2023-10-01 10:45:58 -0700

    VALIDATION: Validator() ReturnsListContainingIs.Data.Frame -> PASS

--- R/Validator.R
+++ R/Validator.R
@@ -1,5 +1,6 @@
 Validator <- \() {
   validators <- list()
-  validators[['Is.Not.NULL']] <- \() {}
+  validators[['Is.Not.NULL']]   <- \() {}
+  validators[['Is.Data.Frame']] <- \() {}
   return(validators)
 }
