a105eb2eb06be659b1e4eb6a98d1554d315a60b7
Flippie Coetser <flippie.coetser@gmail.com>
2023-10-01 08:32:31 -0700

    EXCEPTIONS: Exceptions() ReturnsListOfExceptions -> FAIL

--- tests/testthat/test-Exceptions.R
+++ tests/testthat/test-Exceptions.R
@@ -3,3 +3,13 @@ describe('Exceptions',{
     Exceptions |> expect.exist()
   })
 })
+
+describe('When exceptions <- Exceptions()',{
+  it('then exceptions is a lis',{
+    # When
+    exceptions <- Exceptions()
+
+    # Then
+    exceptions |> expect.list()
+  })
+})
