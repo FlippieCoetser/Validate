a24b4cce5cd2c79ce4583e042629aaf2566f3256
Flippie Coetser <flippie.coetser@gmail.com>
2023-10-01 09:24:24 -0700

    EXCEPTIONS: Exceptions() ReturnsListContainingValue.Invalid -> PASS

--- R/Exceptions.R
+++ R/Exceptions.R
@@ -2,5 +2,6 @@ Exceptions <- \() {
   exceptions <- list()
   exceptions[['Type.Invalid']]  <- \() {}
   exceptions[['Type.Mismatch']] <- \() { }
+  exceptions[['Value.Invalid']] <- \() { }
   return(exceptions)
 }
