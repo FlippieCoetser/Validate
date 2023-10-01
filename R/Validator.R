f0cc54de636f2e65c9ba26bff17403484cc0d023
Flippie Coetser <flippie.coetser@gmail.com>
2023-10-01 10:47:15 -0700

    VALIDATION: Validator() ReturnsListContainingIs.Character -> PASS

--- R/Validator.R
+++ R/Validator.R
@@ -2,5 +2,6 @@ Validator <- \() {
   validators <- list()
   validators[['Is.Not.NULL']]   <- \() {}
   validators[['Is.Data.Frame']] <- \() {}
+  validators[['Is.Character']]  <- \() {}
   return(validators)
 }
